46af8000308082ace7e7e2a24fc66763d0b75582
KSherwoodd <92546532+KSherwoodd@users.noreply.github.com>
2022-11-25 15:13:59 +0000

    2 files modified

--- src/Home/ProfilePage/ProfilePage.tsx
+++ src/Home/ProfilePage/ProfilePage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IonPage } from '@ionic/react';
+import { IonAvatar, IonChip, IonPage, IonContent } from '@ionic/react';
 
 import SideMenu from '../sideMenu';
 import Header from '../mainHeader';
@@ -8,9 +8,36 @@ const Profile = () => (
   <>
     <SideMenu />
     <IonPage className="ion-page" id="main-content">
-        <Header />
+      <Header />
+      <IonContent>
+        <div className="profile-grid-container">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+          <IonAvatar>
+            <img
+              alt="pfp"
+              src="https://ionicframework.com/docs/img/demos/avatar.svg"
+            />
+          </IonAvatar>
+          <div>6</div>
+          <div>7</div>
+          <div>8</div>
+          <div>9</div>
+        </div>
+      </IonContent>
     </IonPage>
   </>
 );
 
 export default Profile;
+
+/*
+        <IonAvatar>
+          <img
+            alt="pfp"
+            src="https://ionicframework.com/docs/img/demos/avatar.svg"
+          />
+        </IonAvatar>
+*/
