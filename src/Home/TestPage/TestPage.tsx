5b0e7c65a7085e2ed7d26fdcdea16b45df1e788c
KSherwoodd <92546532+KSherwoodd@users.noreply.github.com>
2022-11-29 12:01:04 +0000

    2 files modified

--- src/Home/TestPage/TestPage.tsx
+++ src/Home/TestPage/TestPage.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import { IonContent, IonPage, IonAvatar } from '@ionic/react';
+import {
+  IonContent,
+  IonPage,
+  IonAvatar,
+  IonItemSliding,
+  IonItemOptions,
+  IonItemOption,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonImg,
+} from '@ionic/react';
 
 import SideMenu from '../../Home/sideMenu';
 import Header from '../../Home/mainHeader';
 
 import './TestCSS.css';
+
+import { archive, heart, trashBin } from 'ionicons/icons';
 
 const Test = () => {
   return (
@@ -14,11 +27,35 @@ const Test = () => {
         <Header />
         <IonContent>
           <IonAvatar>
-            <img
+            <IonImg
               alt="pfp"
               src="https://ionicframework.com/docs/img/demos/avatar.svg"
             />
           </IonAvatar>
+
+          <IonItemSliding>
+            <IonItemOptions side="start">
+              <IonItemOption color="success">
+                <IonIcon slot="end" icon={archive}></IonIcon>
+                Archive
+              </IonItemOption>
+            </IonItemOptions>
+
+            <IonItem>
+              <IonLabel>Sliding Item with End Icons</IonLabel>
+            </IonItem>
+
+            <IonItemOptions>
+              <IonItemOption>
+                <IonIcon slot="end" icon={heart}></IonIcon>
+                Favorite
+              </IonItemOption>
+              <IonItemOption color="danger">
+                <IonIcon slot="end" icon={trashBin}></IonIcon>
+                Delete
+              </IonItemOption>
+            </IonItemOptions>
+          </IonItemSliding>
         </IonContent>
       </IonPage>
     </>
