1632c98dde332995e94d446f36ce4c3fc180d9cc
KSherwoodd <92546532+KSherwoodd@users.noreply.github.com>
2022-12-02 15:08:40 +0000

    1 file modified

--- src/Home/SignInPage/SignInPage.tsx
+++ src/Home/SignInPage/SignInPage.tsx
@@ -19,11 +19,28 @@ import {
   closeCircleOutline as cross,
 } from 'ionicons/icons';
 
+function checkLogin(username: string, password: string) {
+  //This function will access the database and check login details
+  //Then it will compare the entered data with the provided ones
+  //Then return either a successful login with a userID or a failure
+
+  //For now it will return a bool for testing purposes
+  if (username !== '' && password !== '') {
+    console.log(username);
+    console.log(password);
+    return true;
+  }
+
+  return false;
+}
+
 function SignIn() {
   const [presentAlert] = useIonAlert();
   const [validCheck, setValidCheck] = useState(false);
   const autoFocus = true;
   const inputReq = true;
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
   return (
     <>
@@ -40,6 +57,7 @@ function SignIn() {
                 enterkeyhint="next"
                 mode="ios"
                 required={inputReq}
+                onIonInput={(e: any) => setUsername(e.target.value)}
               >
                 <IonBadge slot="end" color={!validCheck ? 'danger' : 'success'}>
                   <IonIcon icon={!validCheck ? cross : tick} />
@@ -57,6 +75,7 @@ function SignIn() {
                 enterkeyhint="enter"
                 mode="ios"
                 required={inputReq}
+                onIonInput={(e: any) => setPassword(e.target.value)}
               >
                 <IonBadge slot="end" color={!validCheck ? 'danger' : 'success'}>
                   <IonIcon icon={!validCheck ? cross : tick} />
